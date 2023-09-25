38c8c939446a82c31cbf7cb18915aa5bfa530ff9
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-09-26 00:22:38 +0300

    feat: add start command

--- telegram_bot/handlers/start.py
+++ telegram_bot/handlers/start.py
@@ -1,5 +1,8 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Command
+from database import (Database,
+                      get_prompt_add_user,
+                      PROMPT_VIEW_ALL_CHAT_IDS)
 from ..bot_config import dp
 from ..keyboards import main_kb
 
@@ -20,6 +23,16 @@ HELP_TEXT = """
 
 @dp.message_handler(Command('start'))
 async def start(message: types.Message) -> None:
+    user_chat_id = str(message.chat.id)
+    username = message.from_user.username
+    if not username:
+        username = message.from_user.full_name
+
+    db = Database()
+    users = [i['chat_id'] for i in db.get_data_list(PROMPT_VIEW_ALL_CHAT_IDS)]
+    if not user_chat_id in users:
+        db.action(get_prompt_add_user(username, user_chat_id))
+        
     await message.answer(WELCOME, reply_markup=main_kb)
 
 
