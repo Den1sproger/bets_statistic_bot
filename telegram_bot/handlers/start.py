09d574e52f446fedd1f8923292be438f3b75d128
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-09-26 18:07:59 +0300

    feat: add adding to the tables

--- telegram_bot/handlers/start.py
+++ telegram_bot/handlers/start.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Command
+from googlesheets import Stat_mass, Stat_sport_types
 from database import (Database,
                       get_prompt_add_user,
                       PROMPT_VIEW_ALL_CHAT_IDS)
@@ -33,7 +34,15 @@ async def start(message: types.Message) -> None:
 
     if not user_chat_id in users:
         db.action(get_prompt_add_user(username, user_chat_id))
-        
+    
+    # add user to main sheet
+    sm = Stat_mass()
+    sm.add_user(user_chat_id, username)
+
+    # add user to sport types
+    sst = Stat_sport_types()
+    sst.add_user(user_chat_id)
+
     await message.answer(WELCOME, reply_markup=main_kb)
 
 
