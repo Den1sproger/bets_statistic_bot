f17a82f9545756964fba1edd5319a6768b1f7b63
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-09-26 14:15:52 +0300

    feat: add launch file

---
+++ app.py
@@ -0,0 +1,17 @@
+import logging
+
+from aiogram import executor
+from telegram_bot import dp
+from telegram_bot.handlers.start import *
+from telegram_bot.handlers.voting_menu import *
+from telegram_bot.handlers.default_commands import *
+
+
+
+LOG_FILENAME = "/home/tournament_statistic/py_log.log"
+logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, filemode="w")
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
