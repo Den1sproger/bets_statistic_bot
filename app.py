1a3a7ea272abc908b33e3fbb6126b24603ba682b
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-10-21 18:18:07 +0300

    feat: add logging

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import logging
 
 from aiogram import executor
-from telegram_bot import dp, set_default_commands
+from telegram_bot import dp
 from telegram_bot.handlers.start import *
 from telegram_bot.handlers.voting_menu import *
 from telegram_bot.handlers.team_menu import *
@@ -9,15 +9,10 @@ from telegram_bot.handlers.default_commands import *
 
 
 
-# LOG_FILENAME = "/home/tournament_statistic/py_log.log"
-# logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, filemode="w")
-
-async def on_startup(_):
-    await set_default_commands(dp)
+LOG_FILENAME = "/home/statistics/py_log.log"
+logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, filemode="w")
 
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp,
-                           skip_updates=True,
-                           on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=True)
