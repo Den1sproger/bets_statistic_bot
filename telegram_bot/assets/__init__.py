28016f21a7bf35fa2e8d02f9e30126590c39a22b
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-10-23 11:04:45 +0300

    feat: add commad photo

--- telegram_bot/assets/__init__.py
+++ telegram_bot/assets/__init__.py
@@ -1,8 +1,10 @@
 START_PHOTO_PATH = 'telegram_bot/assets/start_photo.jpg'
 VOTING_PHOTO_PATH = 'telegram_bot/assets/voting_photo.jpg'
+TEAM_PHOTO_PATH = 'telegram_bot/assets/team_photo.jpg'
 
 
 __all__ = [
     'START_PHOTO_PATH',
-    'VOTING_PHOTO_PATH'
+    'VOTING_PHOTO_PATH',
+    'TEAM_PHOTO_PATH'
 ]
